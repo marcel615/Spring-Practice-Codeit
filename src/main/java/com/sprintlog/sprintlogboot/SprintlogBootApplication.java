e766b9e80764cb5dcbe0542428b68002af82feff
marcel615 <alswo000615@gmail.com>
2026-07-25 00:41:37 +0900

    Spring Test - Controller 슬라이스 테스트 - @WebMvcTest를 활용한 웹계층 테스트 - @EnableJpaAuditing을 별도의 config 클래스로 분리 - 조회, 생성, 수정, 삭제 관련 여러가지 테스트 메서드 작성

--- src/main/java/com/sprintlog/sprintlogboot/SprintlogBootApplication.java
+++ src/main/java/com/sprintlog/sprintlogboot/SprintlogBootApplication.java
@@ -1,10 +1,10 @@
 package com.sprintlog.sprintlogboot;
 
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
-@EnableJpaAuditing
 @SpringBootApplication
 public class SprintlogBootApplication {
 
